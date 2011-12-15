6e1ff237ea02b7520993703e6d5b9a66cb7b184d
Guillaume Michalag <gm.lelag@gmail.com>
2011-12-15 21:55:29 +0100

    Adding Station Model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meteolog::Application.config.secret_token = 'c7b6aa6aa4020c2c6ba7f13d88f9eaa0baf25e1457f054e27d0afcb002fe1e737d8698eafc6c9946edd091cc9b05e81445c4faad8768a6553fcb5eabca7d9232'
